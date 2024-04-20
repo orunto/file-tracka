292540978b0321bbc27d6d8aa64c3879d224c776
orunto <oruntoeniola@aol.com>
2024-04-21 00:06:44 +0100

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
   return (
     <UserProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </UserProvider>
