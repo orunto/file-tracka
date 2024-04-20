f423fda0f8cf25f38d8d3d81ba2076976058ae77
orunto <oruntoeniola@aol.com>
2024-04-20 23:52:50 +0100

    feat: table creation apis

---
+++ app/api/group-b/route.ts
@@ -0,0 +1,12 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE groupb ( Mda varchar(255), Title varchar(255), Filenumber varchar(11), Fileamount varchar(11), Received varchar(11), Actiontaken varchar(255) );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
