ce8584dc4f9be40cb8729056a957f93b17dc373c
orunto <oruntoeniola@aol.com>
2024-04-26 02:17:10 +0100

    fix: proper development apis and databases created

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -1,10 +1,11 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE groupa ( Mda varchar(255), Title varchar(255), Filenumber varchar(11), Fileamount varchar(11), Received varchar(11), Actiontaken varchar(255) );`;
+      await sql`INSERT INTO Files ( MDA, AssignedGroup , FileTitle, FileNumber, FileAmount, DateAssigned, ActionTaken ) VALUES ('B.I.T', 'A', 'Miscellaneous', '091210121', '100,000,000', '11/11/11', 'Assigned');`;
+
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
