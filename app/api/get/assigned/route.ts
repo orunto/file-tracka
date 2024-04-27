3d5b66c703af4f620008a5f48e149c8cb997734b
orunto <oruntoeniola@aol.com>
2024-04-27 03:03:30 +0100

    fix: database caching disabled

--- app/api/get/assigned/route.ts
+++ app/api/get/assigned/route.ts
@@ -1,7 +1,10 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import { unstable_noStore as noStore } from 'next/cache';
+
 
 export async function GET(request: Request) {
+  noStore();
   try {
     const result = await sql`SELECT MDA, AssignedGroup , FileTitle, FileNumber, FileAmount, DateAssigned, ActionTaken from FILES where actiontaken='Assigned'`;
     return NextResponse.json({ result }, { status: 200 });
